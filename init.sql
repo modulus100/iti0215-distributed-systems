d935c7020071a3f65a1d452f657266d930815aff
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-02-08 10:28:54 +0200

    sql init

--- init.sql
+++ init.sql
@@ -2,3 +2,17 @@ CREATE TABLE IF NOT EXISTS app_user(
     id serial PRIMARY KEY,
     name VARCHAR (255) UNIQUE NOT NULL
 );
+
+CREATE TABLE item(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    app_user_id INT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    PRIMARY KEY(id),
+    CONSTRAINT fk_customer
+        FOREIGN KEY(app_user_id)
+            REFERENCES app_user(id)
+);
+
+INSERT INTO app_user(name) VALUES ('admin');
+INSERT INTO item(app_user_id, name, description) VALUES (1, 'iphone', 'just a phone');
