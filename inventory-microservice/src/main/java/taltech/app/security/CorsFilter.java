4d166f04c58941b0e90fbcbd7dc2a2d2b95b3c42
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-03-07 13:23:36 +0200

    changes for deployment

--- inventory-microservice/src/main/java/taltech/app/security/CorsFilter.java
+++ inventory-microservice/src/main/java/taltech/app/security/CorsFilter.java
@@ -13,7 +13,7 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     @SneakyThrows
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)  {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
